8f3a51fa608ff4b28b54a95b1e1b449370163869
Caspian <caspianrz@gmail.com>
2026-05-30 13:56:52 +0330

    add: building shared and static libraries.
    Signed-off-by: Caspian <caspianrz@gmail.com>

---
+++ cmake/zolal-version.cmake
@@ -0,0 +1,8 @@
+set(zolal_version_major 1)
+set(zolal_version_minor 0)
+set(zolal_version_patch 0)
+
+set(zolal_version 
+	"${zolal_version_major}.${zolal_version_minor}.${zolal_version_patch}")
+
+set(PACKAGE_VERSION ${zolal_version})
